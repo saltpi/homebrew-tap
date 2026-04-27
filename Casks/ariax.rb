d35c7c8c41b278da1af1e5e35fd2fc17569b92f6
endpne <ourfor@qq.com>
2026-04-27 13:49:23 +0800

    chore: update sha256 for ariax 1.0.5

--- Casks/ariax.rb
+++ Casks/ariax.rb
@@ -1,6 +1,6 @@
 cask "ariax" do
   version "1.0.5"
-  sha256 "d91d6c4188ff80ab1bd9f91542b8852e65e5e7b526d69ac93ee747a2e309deb5"
+  sha256 "7031d51d71e6f617a8ef1a5b29b8a247e0ad582f404349a90d1437e001885b88"
 
   url "https://github.com/saltpi/Aria.X/releases/download/#{version}/AriaX.zip"
   name "AriaX"
